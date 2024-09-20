bbbc60aadb51b0f53b9f8a46589e83ecfac5bb32
Jaya <vashisthjaya00@gmail.com>
2024-09-20 17:37:43 +0530

    MongoConnection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.DATABASE, {
       //these are just to get rid of warnings
       useNewUrlParser: true,
       useUnifiedTopology: true,
