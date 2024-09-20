6675cd999f27373e702536409c8435e57c2e30ab
Jaya <vashisthjaya00@gmail.com>
2024-09-20 18:30:52 +0530

    conne

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,7 +6,6 @@ import userRoute from "../routers/userRoutes.js";
 import companyRoute from "../routers/companyRoutes.js";
 import jobRoute from "../routers/jobRoutes.js";
 import applicationRoute from "../routers/applicationRoutes.js";
-import { User } from "../models/userModel.js";
 import mongoose from "mongoose";
 
 dotenv.config({});
