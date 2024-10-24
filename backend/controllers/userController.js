45882fa037fb86d4c6c4dc9f2391d1682d5231cf
Jaya <vashisthjaya00@gmail.com>
2024-10-24 15:17:42 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -117,7 +117,7 @@ export const login = async (req, res) => {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "none",
-        domain: ".vercel.app",
+        secure: true
       })
       .json({
         user: user,
